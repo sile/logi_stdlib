38548747ab70e62ee0c867e42df77972e3b8726b
Takeru Ohta <phjgt308@gmail.com>
2015-11-08 15:07:16 +0900

    Add logi_lib_layout:term_to_iodata/1 function

---
+++ doc/logi_lib_layout.md
@@ -0,0 +1,32 @@
+
+
+# Module logi_lib_layout #
+* [Description](#description)
+* [Function Index](#index)
+* [Function Details](#functions)
+
+Utility functions for logi_layout.
+
+Copyright (c) 2015 Takeru Ohta <phjgt308@gmail.com>
+
+<a name="index"></a>
+
+## Function Index ##
+
+
+<table width="100%" border="1" cellspacing="0" cellpadding="2" summary="function index"><tr><td valign="top"><a href="#term_to_iodata-1">term_to_iodata/1</a></td><td></td></tr></table>
+
+
+<a name="functions"></a>
+
+## Function Details ##
+
+<a name="term_to_iodata-1"></a>
+
+### term_to_iodata/1 ###
+
+<pre><code>
+term_to_iodata(V::term()) -&gt; iodata()
+</code></pre>
+<br />
+
---
+++ src/logi_lib_layout.erl
@@ -0,0 +1,30 @@
+%% @copyright 2015 Takeru Ohta <phjgt308@gmail.com>
+%%
+%% @doc Utility functions for logi_layout
+-module(logi_lib_layout).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported API
+%%----------------------------------------------------------------------------------------------------------------------
+-export([term_to_iodata/1]).
+
+%%----------------------------------------------------------------------------------------------------------------------
+%% Exported Functions
+%%----------------------------------------------------------------------------------------------------------------------
+-spec term_to_iodata(term()) -> iodata().
+term_to_iodata(V) when is_binary(V)    -> V;
+term_to_iodata(V) when is_atom(V)      -> atom_to_list(V);
+term_to_iodata(V) when is_integer(V)   -> integer_to_list(V);
+term_to_iodata(V) when is_float(V)     -> float_to_list(V);
+term_to_iodata(V) when is_function(V)  -> erlang:fun_to_list(V);
+term_to_iodata(V) when is_pid(V)       -> erlang:pid_to_list(V);
+term_to_iodata(V) when is_port(V)      -> erlang:port_to_list(V);
+term_to_iodata(V) when is_reference(V) -> erlang:ref_to_list(V);
+term_to_iodata(V) when is_list(V)      ->
+    IsChar = fun (C) -> is_integer(C) andalso 0 =< C andalso C =< 16#10ffff end,
+    case lists:all(IsChar, V) of
+        true  -> V;
+        false -> io_lib:format("~1000000p", [V])
+    end;
+term_to_iodata(V) ->
+    io_lib:format("~1000000p", [V]).
